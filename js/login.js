12476a1a201143209ddbb99ace72ad80e90cde05
valeria-abraham <valeriabraham@gmail.com>
2021-08-20 11:04:52 -0300

    Armado de login en js y html (sin formato)

--- js/login.js
+++ js/login.js
@@ -1,3 +1,15 @@
+function validar(){
+    let nombre=document.getElementById("user").value;
+    let password=document.getElementById("pwd").value;
+
+    if(nombre!=""&&password!=""){
+        window.location.href = "home.html";
+    }else{
+        alert("Poner usuario y contraseña")
+    }
+    
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.

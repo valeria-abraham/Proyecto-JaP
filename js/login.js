fb1ac763f6290542de5719a86043dd1cbe4dc9fa
valeria-abraham <valeriabraham@gmail.com>
2021-08-20 23:41:45 -0300

    Logo en la pestaña de la página

--- js/login.js
+++ js/login.js
@@ -1,10 +1,14 @@
 function validar(){
+// obtengo del formulario los valores de los elementos nombre y contraseña para validar
     let nombre=document.getElementById("user").value;
     let password=document.getElementById("pwd").value;
-
+//para validar la condicion es que no este vacio ningun campo
+//uso .trim() para evitar que se tome como valido ingresar solo espacios sin ningun caracter
     if(nombre.trim()!=""&&password.trim()!=""){
+        //llevo a la página home que sería el inicio una vez ingresado el usuario
         window.location.href = "home.html";
     }else{
+        //si no se pone nada aparece una alerta
         alert("Poner usuario y contraseña")
     }
     

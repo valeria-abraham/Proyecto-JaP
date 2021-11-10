53f8989c2594a0c34615cb8b7de4baf3c3ae6ef9
valeria-abraham <valeriabraham@gmail.com>
2021-11-10 11:48:32 -0300

    Entrega 7 completa

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,7 @@
 const MONEDA = "UYU"; // Defino la moneda con la que quiero trabajar
-let carrito = []; // Para cargar los productos en el carrito
+var carrito = []; // Para cargar los productos en el carrito
 var carrito_length = 0;
+var card = [];
 
 function showCarrito(carrito) {
   // Función que muestra los productos en el carrito
@@ -8,10 +9,12 @@ function showCarrito(carrito) {
   for (let i = 0; i < carrito.articles.length; i++) {
     // Recorro todos los artículos del json
     let art = carrito.articles[i];
+
     let ex = exchange(art.currency, MONEDA, parseFloat(art.unitCost)); // Llamo a la función que hace la conversión de moneda
     // Vemos en el contenido para unir al código HTML, a cada input (para poner la cantidad) le asigno un id que
     // corresponde con su orden en el json, además llamo a la función update() cuando se cambia el valor (onchange)
     // Muestro también el nombre, la foto y el precio unitario del producto
+
     html +=
       `
         <div class="list-group-item list-group-item-action mt-2" style="border:1px solid white">
@@ -43,8 +46,11 @@ function showCarrito(carrito) {
       art.count +
       `" min = 1 onchange="update(` +
       i +
-      `)">
-                        </span>
+      `)">                  
+      </span>
+      <button type="button" class="btn btn-anchor" onclick="removeItemCart(` +
+      i +
+      `)"> Quitar </button> 
                     </div>
                 </div>
                 <div class="col-3">
@@ -146,15 +152,75 @@ function update(articulo) {
 }
 
 function savePayment() {
+  // Función que guarda el tipo de pago y lo muestra
   let html = "";
   let selected = document.getElementsByName("payment");
   let pago = 0;
   if (selected[0].checked) {
     html = `<button type="button" class="btn-non-format2 disabled">Ha seleccionado el pago por transferencia bancaria.</button>`;
     pago = selected[0].value;
+    modal = `<div class="row mt-2">
+    <div class="col-md-12">
+      <div class="card bg-crema mb-1">
+        <div class="card-body text-center">
+          <p> El número de cuenta donde depositar es: <strong>1234567890</strong> </p>
+        </div>
+      </div>
+    </div>
+  </div>`;
+    document.getElementById("modal-payment").innerHTML = modal;
   } else if (selected[1].checked) {
     html = `<button type="button" class="btn-non-format2 disabled">Ha seleccionado el pago por tarjeta de crédito.</button>`;
     pago = selected[1].value;
+    modal = `<div class = "row">
+  <div class="col-md-12 f-w-600 mb-2">
+  <label class="labels">Número de tarjeta:</label
+  ><input
+    type="text"
+    class="form-control"
+    placeholder="xxxx - xxxx - xxxx - xxxx"
+    id="card-number"
+    value=""
+    autocomplete="off"
+  />
+</div>
+<div class="col-md-12 f-w-600 mb-2">
+  <label class="labels">CVV:</label
+  ><input
+    type="text"
+    class="form-control"
+    placeholder="xxx (detrás de la tarjeta)"
+    id="cvv"
+    value=""
+    autocomplete="off"
+  />
+</div>
+<div class="col-md-12 f-w-600 mb-2">
+  <label class="labels">Fecha de vencimiento:</label
+  ><input
+    type="month"
+    class="form-control"
+    placeholder="noviembre-2021"
+    id="valid-date"
+    value=""
+    autocomplete="off"
+  />
+</div>
+<div class="col-md-12 f-w-600 mb-2">
+  <label class="labels">Titular de la tarjeta:</label
+  ><input
+    type="text"
+    class="form-control"
+    placeholder="Valeria Abraham"
+    id="card-name"
+    value=""
+    autocomplete="off"
+  />
+</div>
+</div>
+<button type="button" class="btn btn-tomato" onclick="validatePayment()"> Verificar </button> 
+<div id="payment-alert"></div>`;
+    document.getElementById("modal-payment").innerHTML = modal;
   } else {
     html = `<button type="button" class="btn-non-format2 disabled">No se ha seleccionado una forma de pago.</button>`;
     pago = 0;
@@ -163,7 +229,42 @@ function savePayment() {
   document.getElementById("payment").innerHTML = html;
 }
 
+function validatePayment() {
+  if (
+    document.getElementById("card-number").value != "" &&
+    document.getElementById("valid-date").value != "" &&
+    document.getElementById("cvv").value != "" &&
+    document.getElementById("card-name").value != ""
+  ) {
+    html = `<div class="row mt-2">
+    <div class="col-md-12">
+      <div class="card bg-not-tomato mb-1">
+        <div class="card-body text-center">
+          Ha quedado registrada su tarjeta para realizar esta compra, puede finalizarla haciendo click en <strong>Cerrar</strong> y luego en <strong>Finalizar compra</strong>.
+        </div>
+      </div>
+    </div>
+  </div>`;
+    document.getElementById("payment-alert").innerHTML = html;
+    return 0
+  } else {
+    html = `<div class="row mt-2">
+    <div class="col-md-12">
+      <div class="card bg-tomato mb-1">
+        <div class="card-body text-center">
+        Faltan completar datos de su tarjeta 
+          
+        </div>
+      </div>
+    </div>
+  </div>`;
+    document.getElementById("payment-alert").innerHTML = html;
+    return 1
+  }
+}
+
 function shipping() {
+  // Función que calcula y devuelve el costo de envío según el botón apretado
   let selected = document.getElementsByName("envio-precio");
   let envio = 0;
   for (i = 0; i < selected.length; i++) {
@@ -175,6 +276,7 @@ function shipping() {
 }
 
 function clearPayment() {
+  // Función que borra el tipo de pago elegido
   let selected = document.getElementsByName("payment"); // Para limpiar la selección de pago
   for (
     i = 0;
@@ -187,18 +289,34 @@ function clearPayment() {
     // Para borrar la información del pago cada vez que recargo la página
     localStorage.removeItem("payment");
   }
-  document.getElementById("payment").innerHTML = `<button type="button" class="btn-non-format2 disabled">No se ha seleccionado una forma de pago.</button>`;
+  document.getElementById("modal-payment").innerHTML = "";
+  document.getElementById(
+    "payment"
+  ).innerHTML = `<button type="button" class="btn-non-format2 disabled">No se ha seleccionado una forma de pago.</button>`;
 }
 
 function validation() {
-  let envio = shipping();
-  let payment = localStorage.getItem("payment");
-  let calle = document.getElementById("calle").value;
+  // Función que valida los datos ingresados para finalizar la compra
+  // Llamamos a todos los campos donde debería haber algo elegido
+  let envio = shipping(); // La función devuelve 0 si no se apretó ningún botón
+  let payment = localStorage.getItem("payment"); // Guardamos en el localStorage el tipo de pago como un número, si no se eligió se devuelve null
+  let calle = document.getElementById("calle").value; // Elementos de los input de la dirección de envío
   let num = document.getElementById("num-puerta").value;
   let esq = document.getElementById("esq").value;
   let pais = document.getElementById("pais").value;
   let html = "";
-  if (envio != 0 && payment != null && calle != "" && num != "" && esq != "" && pais !="") {
+  if (payment === 2 && validatePayment() === 1) {
+    payment = null;
+  }
+  if (
+    envio != 0 &&
+    payment != null &&
+    calle != "" &&
+    num != "" &&
+    esq != "" &&
+    pais != ""
+  ) {
+    // Si se completaron todos los datos requeridos mostramos que la compra se realizó con éxito
     html = `<div class="row mt-2">
     <div class="col-md-12">
       <div class="card bg-not-tomato mb-1">
@@ -210,6 +328,7 @@ function validation() {
   </div>`;
     document.getElementById("not-complete-alert").innerHTML = html;
   } else {
+    // Si hay alguno que no se haya rellenado mostramos que falta algo para llenar
     html = `<div class="row mt-2">
     <div class="col-md-12">
       <div class="card bg-tomato mb-1">
@@ -223,6 +342,19 @@ function validation() {
   }
 }
 
+function removeItemCart(id) {
+  // Función que remueve el item del carrito deseado
+  let i = 0;
+  for (let art of carrito.articles) {
+    art.count = document.getElementById(i).value; // Guardamos la cantidad que tiene en el momento de borrar
+    i++;
+  }
+  carrito.articles.splice(id, 1); // Sacamos el elemento dado del array
+  carrito_length = carrito.articles.length; // Para que ande el showTotal()
+  showCarrito(carrito);
+  showTotal();
+}
+
 document.addEventListener("DOMContentLoaded", function (e) {
   if (localStorage.getItem("payment") != null) {
     // Para borrar la información del pago cada vez que recargo la página
@@ -230,7 +362,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
   }
   getJSONData("https://japdevdep.github.io/ecommerce-api/cart/654.json").then(
     function (resultObj) {
-      // obtengo el json del producto
+      // Obtengo el json del producto
       if (resultObj.status === "ok") {
         carrito = resultObj.data;
         carrito_length = carrito.articles.length; // Para no tener problemas llamando a la función showTotal() en cualquier lado
